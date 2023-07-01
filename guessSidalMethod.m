f4aa4bdea598c8488c3ee063f2407d5ba676f9ed
Anamul Haque Sayem <88246699+AH-sayem@users.noreply.github.com>
2023-07-02 01:52:31 +0600

    Add files via upload

--- guessSidalMethod.m
+++ guessSidalMethod.m
@@ -10,12 +10,17 @@ j=1;
 while abs(Y-x)>e
     Y=x;
     for i=1:n
+      %  for k=1:n
         x(i)=(B(i)/A(i,i))-(A(i,[1:i-1,i+1:n])*P([1:i-1,i+1:n]))/A(i,i);
         P(i)=x(i);
+      %  end 
     end
-    fprintf('iteration no %d\n',j)
-    x;
-    I=i;
-    j=j+1;
-   % disp(i', x');
-end  
+    
+    fprintf('iteration no %d\n',j) ;
+    x 
+    j=j+1 ;
+    
+end 
+fprintf('\n Total itteration= %d \n Roots are:\n  a=%d \n b=%d \n c=%d\n',j,x(1),x(2),x(3));
+
+    
